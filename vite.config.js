903c7556b4b87e4a7f6ccd010251d3c6ef98b5cd
88warren <warren_laura@yahoo.co.uk>
2025-05-20 13:59:05 +0100

    Update routes

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig(({ mode }) => {
       sourcemap: false,
       chunkSizeWarningLimit: 500,
       assetsInlineLimit: 0,
-      assetsDir: 'images',
+      assetsDir: 'assets',
     },
     define: {
       "process.env.NODE_ENV": JSON.stringify(mode),
