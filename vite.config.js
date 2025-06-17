9f6e19aae856e1c2bdb9444e6978dc4636dd27e5
88warren <warren_laura@yahoo.co.uk>
2025-06-17 10:11:05 +0100

    Tidying Code and comments

--- vite.config.js
+++ vite.config.js
@@ -49,12 +49,6 @@ export default defineConfig(({ mode }) => {
       assetsInlineLimit: 0,
       assetsDir: 'assets',
     },
-  //  define: {
-  //     "process.env.NODE_ENV": JSON.stringify(mode),
-  //     // Make environment variables available in client code
-  //     "import.meta.env.VITE_BACKEND_URL": JSON.stringify(process.env.VITE_BACKEND_URL),
-  //     "import.meta.env.VITE_RECAPTCHA_SITE_KEY": JSON.stringify(process.env.VITE_RECAPTCHA_SITE_KEY),
-  //   },
     base: "/", 
   };
 });
